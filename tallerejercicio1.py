a01b3955db20636489b378a6a9499a837ec0d5ae
johan1033gf <johangonzalez.m38@gmail.com>
2024-02-13 09:38:30 -0500

    ejercisisos tarea

--- tallerejercicio1.py
+++ tallerejercicio1.py
@@ -1,3 +1,35 @@
 #diccionario ejercisio
-def calcularnots(notas)
-    return sum(notas) /len(notas)
+def Ingresaralumno():
+    Nombre = input("Ingrese el nombre del alumno: ")
+    Notas = []
+    while True:
+        Nota = float(input("Ingrese la nota del alumno (ingrese un número negativo para registrar): "))
+        if Nota < 0:
+            break
+        Notas.append(Nota)
+    return Nombre, Notas
+
+def Calcularpromedio(Notas):
+    return sum(Notas) / len(Notas)
+
+def main():
+    Alumnos = {}
+    while True:
+        Cantidadalumnos = int(input("Ingrese la cantidad de alumnos que desea ingresar (ingrese un número negativo para registrar): "))
+        if Cantidadalumnos < 0:
+            break
+        for _ in range(Cantidadalumnos):
+            Nombre, Notas = Ingresaralumno()
+            if Nombre in Alumnos:
+                print("Error: El nombre del alumno ya existe.")
+            else:
+                Alumnos[Nombre] = Notas
+                print("Alumno ingresado correctamente.")
+        print("\n")
+    print("Lista de alumnos con promedio:")
+    for Nombre, Notas in Alumnos.items():
+        Promedio = Calcularpromedio(Notas)
+        print(f"{Nombre}: {Promedio}")
+
+if __name__ == "__main__":
+    main()
